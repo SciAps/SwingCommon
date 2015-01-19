fcb4acf517d64b9bf203231cf8d9f433f9967e84
Paul Soucy <paul@powerdesk.(none)>
2015-01-18 23:33:47 -0500

    added series implementaton for LabeledXYDataset

--- src/main/java/com/sciaps/common/swing/view/LabeledXYDataset.java
+++ src/main/java/com/sciaps/common/swing/view/LabeledXYDataset.java
@@ -8,56 +8,65 @@ import org.jfree.data.xy.AbstractXYDataset;
  *
  * @author sgowen
  */
-public final class LabeledXYDataset extends AbstractXYDataset
-{
-    private final Comparable _key;
-    private List<Double> _x = new ArrayList();
-    private List<Double> _y = new ArrayList();
-    private List<String> _label = new ArrayList();
+public final class LabeledXYDataset extends AbstractXYDataset {
 
-    public LabeledXYDataset(Comparable key) {
-        _key = key;
+    public static class LabeledXYSeries {
+        private Comparable _key;
+
+        private List<Double> _x = new ArrayList();
+        private List<Double> _y = new ArrayList();
+        private List<String> _label = new ArrayList();
+
+        public LabeledXYSeries(Comparable key) {
+            _key = key;
+        }
+
+        public void add(double x, double y, String label) {
+            _x.add(x);
+            _y.add(y);
+            _label.add(label);
+        }
+
     }
 
-    public void add(double x, double y, String label)
-    {
-        _x.add(x);
-        _y.add(y);
-        _label.add(label);
+    private ArrayList<LabeledXYSeries> _series = new ArrayList<LabeledXYSeries>();
+
+    public void addSeries(LabeledXYSeries series) {
+        _series.add(series);
+        fireDatasetChanged();
     }
 
-    public String getLabel(int series, int item)
-    {
-        return _label.get(item);
-    }
-
-    @Override
-    public int getSeriesCount()
-    {
-        return 1;
-    }
-
-    @Override
-    public Comparable getSeriesKey(int series)
-    {
-        return _key;
-    }
-
-    @Override
-    public int getItemCount(int series)
-    {
-        return _label.size();
+    public void removeSeries(LabeledXYSeries series) {
+        _series.remove(series);
+        fireDatasetChanged();
     }
 
     @Override
-    public Number getX(int series, int item)
-    {
-        return _x.get(item);
+    public int getSeriesCount() {
+        return _series.size();
     }
 
     @Override
-    public Number getY(int series, int item)
-    {
-        return _y.get(item);
+    public Comparable getSeriesKey(int series) {
+        return _series.get(series)._key;
+    }
+
+    @Override
+    public int getItemCount(int series) {
+        return _series.get(series)._x.size();
+    }
+
+    @Override
+    public Number getX(int series, int item) {
+        return _series.get(series)._x.get(item);
+    }
+
+    @Override
+    public Number getY(int series, int item) {
+        return _series.get(series)._y.get(item);
+    }
+
+    public String getLabel(int series, int item) {
+        return _series.get(series)._label.get(item);
     }
 }
