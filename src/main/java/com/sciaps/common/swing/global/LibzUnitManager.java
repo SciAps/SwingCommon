484aba994915153c0bb554c6f72269c97d4c0955
john chen <johnchenri@gmail.com>
2015-09-30 12:27:31 -0400

    Speedup initial loading time

--- src/main/java/com/sciaps/common/swing/global/LibzUnitManager.java
+++ src/main/java/com/sciaps/common/swing/global/LibzUnitManager.java
@@ -22,7 +22,6 @@ import java.util.*;
 public class LibzUnitManager {
 
     static Logger logger = LoggerFactory.getLogger(LibzUnitManager.class);
-    private final String WIP_LOADING_MSG = "Loading Data ...";
     
     public Instrument instrument;
     public CalibrationShotManager shotManager;
@@ -49,9 +48,6 @@ public class LibzUnitManager {
         }
     });
 
-    private ArrayList<Model> mCalibrationModelList = new ArrayList<Model>();
-
-
     public LibzUnitManager() {
         recreateCache();
     }
@@ -77,8 +73,6 @@ public class LibzUnitManager {
     public void onPullEvent(PullEvent pullEvent) {
         recreateCache();
         mObjTracker.clear();
-
-        preLoadCalibrationModels();
     }
 
     @Subscribe
@@ -93,10 +87,6 @@ public class LibzUnitManager {
             case DBObjEvent.CREATED:
                 mTestsOfStandard.insert(modifiedEvent.obj);
                 mTestsByTime.insert(modifiedEvent.obj);
-
-                if (modifiedEvent.obj instanceof Model) {
-                    mCalibrationModelList.add((Model) modifiedEvent.obj);
-                }
                 break;
 
             case DBObjEvent.MODIFIED:
@@ -107,10 +97,6 @@ public class LibzUnitManager {
             case DBObjEvent.DELETED:
                 mTestsOfStandard.delete(modifiedEvent.obj);
                 mTestsByTime.delete(modifiedEvent.obj);
-
-                if (modifiedEvent.obj instanceof Model) {
-                    mCalibrationModelList.remove(modifiedEvent.obj);
-                }
                 break;
         }
     }
@@ -181,119 +167,4 @@ public class LibzUnitManager {
         }
     }
 
-    public ArrayList<Model> getModelsForStandard(Standard standard) {
-
-        ArrayList<Model> models = new ArrayList<Model>();
-
-        // save a list of existing models
-        if (mCalibrationModelList.isEmpty()) {
-            loadCalibrationModels();
-        }
-
-        for(Model model : mCalibrationModelList) {
-            if (model.standardList.contains(standard)) {
-                models.add(model);
-            }
-        }
-
-        return models;
-    }
-
-    public ArrayList<Model> getModelsForRegion(Region theRegion) throws Exception {
-
-        ArrayList<Model> models = new ArrayList<Model>();
-
-        // save a list of existing models
-        if (mCalibrationModelList.isEmpty()) {
-            loadCalibrationModels();
-        }
-
-        for (Model model : mCalibrationModelList) {
-
-            for (IRCurve curve : model.irs.values()) {
-                mObjLoader.deepLoad(curve);
-
-                for (Region region : curve.numerator) {
-                    if (region == theRegion) {
-
-                        // add it if not already added
-                        if (models.contains(model) == false) {
-                            models.add(model);
-                        }
-                    }
-                }
-
-                for (Region region : curve.denominator) {
-                    if (region == theRegion) {
-
-                        // add it if not already added
-                        if (models.contains(model) == false) {
-                            models.add(model);
-                        }
-                    }
-                }
-            }
-        }
-        return models;
-    }
-
-    private void loadCalibrationModels() {
-        try {
-
-            // copy from a local list to ensure we fully loaded all existing calibration
-            ArrayList<Model> localList = new ArrayList<Model>();
-
-            Collection<String> modelIds = mApiHandler.getAllIds(Model.class);
-            for (String modelId : modelIds) {
-                Model model = mObjLoader.deepLoad(Model.class, modelId);
-                if (model != null) {
-                    localList.add(model);
-                }
-            }
-
-            mCalibrationModelList.clear();
-            // copy from a local list to ensure we fully loaded all existing calibration
-            mCalibrationModelList.addAll(localList);
-
-        } catch (Exception e) {
-            logger.error("Exception loading calibration: " + e.getMessage());
-            // if anything happen, clear and try reload in later time
-            mCalibrationModelList.clear();
-        }
-    }
-
-    private void preLoadCalibrationModels() {
-
-        // Load the calibration in the background
-        Runnable deepLoadRunnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    mGlobalEventBus.post(new WorkInProgressEvent(WIP_LOADING_MSG, false));
-
-                    loadCalibrationModels();
-
-                    for (Model model : mCalibrationModelList) {
-                        for (IRCurve curve : model.irs.values()) {
-                            try {
-                                mObjLoader.deepLoad(curve);
-                            } catch (Exception e) {
-                                // its ok if exception here, just reload in later time
-                            }
-                        }
-                    }
-
-                    mGlobalEventBus.post(new WorkInProgressEvent(WIP_LOADING_MSG, true));
-                } catch (Exception e) {
-
-                    logger.error("Exception in pre-loading calibration: " + e.getMessage());
-                    // if anything happen, clear and try reload in later time
-                    mCalibrationModelList.clear();
-                }
-            }
-        };
-
-        Thread deeploadThread = new Thread(deepLoadRunnable);
-        deeploadThread.start();
-    }
 }
