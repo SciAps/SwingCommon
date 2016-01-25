2e8fd310834ebc5bd5a1e7ecb641580d7ac85050
john chen <johnchenri@gmail.com>
2016-01-25 18:25:45 -0500

    PB UI redeign

---
+++ src/main/java/com/sciaps/common/swing/events/SetDisplayEvent.java
@@ -0,0 +1,11 @@
+package com.sciaps.common.swing.events;
+
+
+public class SetDisplayEvent {
+
+    public final int mDisplayID;
+
+    public SetDisplayEvent(int displayID) {
+        mDisplayID = displayID;
+    }
+}
